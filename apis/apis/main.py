d69dd0d5b153686a2571dcb3af950774b70c1063
awsconsultant9 <104348997+awsconsultant9@users.noreply.github.com>
2025-07-16 23:49:02 +0530

    Update main.py

--- apis/apis/main.py
+++ apis/apis/main.py
@@ -10,7 +10,7 @@ from fastapi.responses import JSONResponse
 # from .db.models import User
 # from redis import Redis
 
-'''
+///
 redis_client = Redis(host="localhost", port=6379, db=0, decode_responses=True)
 RATE_LIMIT = 5
 TIME_WINDOW = 60
